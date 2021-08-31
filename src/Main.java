1a7ad7b7abc49545caabc37dd580e506e37da13a
Nathan <nathan.caron@natchi.fr>
2021-08-31 13:51:10 +0200

    Méthode switch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 class Main {
 
   public static void main(String[] args) {
-    int i = 10;
+    int note = 0;
 
-    if (i >= 10) System.out.println("Test");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Note de Nathan: ");
+    note = sc.nextInt();
+    sc.close();
+
+    switch (note) {
+      case 0:
+        System.out.println("Ca fait mal");
+        break;
+      case 10:
+        System.out.println("La moyenne, ça passe");
+        break;
+      case 20:
+        System.out.println("BIEN JOUE");
+        break;
+      default:
+        System.out.println("Il faut travailler");
+        break;
+    }
   }
 }
