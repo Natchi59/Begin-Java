00a738e048ec8ab9c5cfdc29447fed9cf0e4680d
Nathan <nathan.caron@natchi.fr>
2021-09-01 13:51:10 +0200

    do/while

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 class Main {
 
   public static void main(String[] args) {
-    int a = 0, b = 15;
+    int a = 1, b = 1;
+
+    do {
+      System.out.println(a);
+    } while (a < b);
 
     while (a < b) {
-      System.out.println("a = " + a);
-      a++;
+      System.out.println(a);
     }
   }
 }
