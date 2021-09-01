b454e7fbaff8363ca1ca13df3fe85255663f6740
Nathan <nathan.caron@natchi.fr>
2021-09-01 13:45:19 +0200

    Boucle

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,11 @@
-import java.util.Scanner;
-
 class Main {
 
   public static void main(String[] args) {
-    int note = 0;
+    int a = 0, b = 15;
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Note de Nathan: ");
-    note = sc.nextInt();
-    sc.close();
-
-    switch (note) {
-      case 0:
-        System.out.println("Ca fait mal");
-        break;
-      case 10:
-        System.out.println("La moyenne, ça passe");
-        break;
-      case 20:
-        System.out.println("BIEN JOUE");
-        break;
-      default:
-        System.out.println("Il faut travailler");
-        break;
+    while (a < b) {
+      System.out.println("a = " + a);
+      a++;
     }
   }
 }
